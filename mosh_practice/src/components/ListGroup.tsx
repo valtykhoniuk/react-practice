f65d23a61fd117edc72e240e81eceda96f789060
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-05-06 19:00:27 +0200

    [mosh_practice]: onClick method

--- mosh_practice/src/components/ListGroup.tsx
+++ mosh_practice/src/components/ListGroup.tsx
@@ -1,5 +1,9 @@
+import type { MouseEvent } from "react";
+
 const ListGroup = () => {
   let items = ["NewYork", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
@@ -8,7 +12,7 @@ const ListGroup = () => {
       {items.length === 0 && <h1>no item found</h1>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item} className="list-group-item">
+          <li key={item} className="list-group-item" onClick={handleClick}>
             {item}
           </li>
         ))}
