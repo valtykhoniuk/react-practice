3c2141d8364a661a81dcd8b881c2b2bf433da350
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-05-06 20:03:56 +0200

    [mosh_practice]: children

--- mosh_practice/src/App.tsx
+++ mosh_practice/src/App.tsx
@@ -1,19 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["NewYork", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>world </span>
+      </Alert>
     </div>
   );
 }
--- mosh_practice/src/components/Alert.tsx
+++ mosh_practice/src/components/Alert.tsx
@@ -1,5 +1,11 @@
-const Alert = () => {
-  return <div></div>;
+import type { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
+  return <div className="alert alert-primary">{children}</div>;
 };
 
 export default Alert;
