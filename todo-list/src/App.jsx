79d7bd6b914a70370899e2e8f6bb81b2faf785ae
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-05-08 22:26:10 +0200

    [todo-list]: basic jsx added

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -2,7 +2,34 @@ import React from "react";
 import "./styles.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <form className="new-tem-form">
+        <div className="form-row">
+          <label>New item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo list</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default App;
