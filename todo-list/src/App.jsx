5255d773d06618bdd72670cead4d453520392aca
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-05-09 09:55:15 +0200

    [todo-list]: delete and onChange input logic added

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -17,6 +17,24 @@ const App = () => {
     setNewItem("");
   }
 
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-tem-form">
@@ -37,9 +55,19 @@ const App = () => {
           return (
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
                 {todo.title}
               </label>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
             </li>
           );
         })}
