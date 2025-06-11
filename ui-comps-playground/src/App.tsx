9c53b2fd2eb878b275e779485bf2e31d1f2e7af1
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-06-11 11:28:16 +0200

    added Tabs

--- ui-comps-playground/src/App.tsx
+++ ui-comps-playground/src/App.tsx
@@ -1,10 +1,12 @@
 import Modal from "./components/Modal";
+import Tabs from "./components/Tabs";
 
 function App() {
   return (
     <div className="p-4">
       <h1 className="text-2xl mb-4">Components Playground</h1>
       <Modal />
+      <Tabs />
     </div>
   );
 }
---
+++ ui-comps-playground/src/components/Tabs.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const Tabs = () => {
+  const [activeTab, setActiveTab] = useState(0);
+  const tabs = ["Tab 1", "Tab 2"];
+  const content = ["Content 1", "Content 2"];
+
+  return (
+    <>
+      <div className="flex gap-2 mt-3">
+        {tabs.map((tab, i) => (
+          <button
+            className="border p-2"
+            key={i}
+            onClick={() => setActiveTab(i)}
+          >
+            {tab}
+          </button>
+        ))}
+      </div>
+      <div className="mt-4">{content[activeTab]}</div>
+    </>
+  );
+};
+
+export default Tabs;
